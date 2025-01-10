059609654a815ea1844fc68cb5a4b36e6344065a
lulek1410 <jan.szewczynski@gmail.com>
2025-01-10 20:49:20 +0100

    (ref): Inject the password encoder in user config instead of creating a new one

--- src/main/java/com/example/shopping_list/user/UserConfig.java
+++ src/main/java/com/example/shopping_list/user/UserConfig.java
@@ -5,16 +5,19 @@ import java.util.List;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import lombok.RequiredArgsConstructor;
+
 @Configuration
+@RequiredArgsConstructor
 public class UserConfig {
+
+  private final PasswordEncoder encoder;
 
   @Bean
   CommandLineRunner commandLineRunner(UserRepository repository) {
     return args -> {
-      PasswordEncoder encoder = new BCryptPasswordEncoder();
       User mike = new User("mike.example@mail.com", encoder.encode("simplePassphrase123%"), "Mike", "Jones");
       User angelika = new User("ann.mirco@mail.com", encoder.encode("abcd123#"), "Angelika", "Mirco");
       repository.saveAll(List.of(mike, angelika));
